a93a2fcf8043af414510404172bd7c1b2147582c
Rinkal <rinkal.bhingradiya@checker-solutions.com>
2023-04-05 10:45:17 +0530

    - Completed new dashboard UI - Completed new dashboard UI for when no job available

---
+++ app/src/main/java/com/mor/sa/android/activities/NewDashboardScreenActivity.java
@@ -0,0 +1,14 @@
+package com.mor.sa.android.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NewDashboardScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_dashboard_screen);
+    }
+}
